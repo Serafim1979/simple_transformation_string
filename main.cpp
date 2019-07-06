466827a221e34652a6db4e158d6855e5325d7d61
Serafim1979 <korolkov.ivan@mail.ru>
2019-07-06 19:10:47 +0400

    simple transformation string

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main()
+{
+	cout << "Enter a word: ";
+	string word;
+	cin >> word;
+
+	char temp;
+	int i, j;
+
+	for (j = 0, i = word.size() - 1; j < i; --i, ++j)
+	{
+		temp = word[i];
+		word[i] = word[j];
+		word[j] = temp;
+	}
+
+	cout << word << "\nDone\n";
+
+	system("pause");
+	return 0;
+}
